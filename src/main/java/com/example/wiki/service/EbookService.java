9b43dd45111321fbd1e77b7f84442b3e5beccfec
admin <yf193557902@163.com>
2023-01-08 16:47:22 +0800

    3.6 CopyUtil封装

--- src/main/java/com/example/wiki/service/EbookService.java
+++ src/main/java/com/example/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.example.wiki.mapper.DemoMapper;
 import com.example.wiki.mapper.EbookMapper;
 import com.example.wiki.req.EbookReq;
 import com.example.wiki.resp.EbookRsp;
+import com.example.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,13 +28,19 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
 
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookRsp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookRsp ebookRsp = new EbookRsp();
-            //实现对象的复制
-            BeanUtils.copyProperties(ebook,ebookRsp);
-            respList.add(ebookRsp);
-        }
-        return respList;
+
+//        List<EbookRsp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+////            EbookRsp ebookRsp = new EbookRsp();
+////            //实现对象的复制
+////            BeanUtils.copyProperties(ebook,ebookRsp);
+//            //实现单个对象的赋值
+//            EbookRsp ebookRsp = CopyUtil.copy(ebook, EbookRsp.class);
+//            respList.add(ebookRsp);
+//        }
+
+        //实现列表的赋值
+        List<EbookRsp> list = CopyUtil.copyList(ebookList, EbookRsp.class);
+        return list;
     }
 }
