3b82cba20ff3d100ca46333a310ad164abd1b45b
admin <yf193557902@163.com>
2023-01-08 11:07:32 +0800

    2-6 开发hello world接口，访问地址为http://127.0.0.1:8880/hello

--- src/main/java/com/example/wiki/WikiApplication.java
+++ src/main/java/com/example/wiki/WikiApplication.java
@@ -6,8 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan({"com.example"}) //扫描包
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
---
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.wiki.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    //405 请求方式不支持
+    //404 请求访问不到
+    @GetMapping("/hello")
+//    @RequestMapping("/hello") //表示支持所有的请求方式
+    public String hello(){
+        return "hello world";
+    }
+}
